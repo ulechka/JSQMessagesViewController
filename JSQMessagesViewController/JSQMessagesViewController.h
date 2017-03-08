806b5b50f4f301f0e70e67870357ba799a28a5b9
Ulechka Khmeleva <ulechka.hmeleva@gmail.com>
2017-03-08 22:25:11 +0700

    made framework

---
+++ JSQMessagesViewController/JSQMessagesViewController.h
@@ -0,0 +1,19 @@
+//
+//  JSQMessagesViewController.h
+//  JSQMessagesViewController
+//
+//  Created by Uliana Khmeleva on 08/03/2017.
+//  Copyright © 2017 UtasProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSQMessagesViewController.
+FOUNDATION_EXPORT double JSQMessagesViewControllerVersionNumber;
+
+//! Project version string for JSQMessagesViewController.
+FOUNDATION_EXPORT const unsigned char JSQMessagesViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSQMessagesViewController/PublicHeader.h>
+
+
